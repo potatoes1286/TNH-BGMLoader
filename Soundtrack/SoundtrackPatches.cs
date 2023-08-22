771aa9f98642d9bd208f1765b8e5f661fe1086b4
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2023-08-22 17:08:19 -0400

    Add OrbTouch

--- Soundtrack/SoundtrackPatches.cs
+++ Soundtrack/SoundtrackPatches.cs
@@ -1,37 +1,41 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using FistVR;
 using HarmonyLib;
 using UnityEngine;
 
 namespace TNHBGLoader.Soundtrack {
 	public class SoundtrackPatches {
+		private static HoldData? HoldMusic;
+		
 		[HarmonyPatch(typeof(FVRFMODController), "SwitchTo")]
 		[HarmonyPrefix]
 		public static bool Patch_SwitchTo_PlaySoundtrackSongs(ref int musicIndex, ref float timeDelayStart, ref bool shouldStop, ref bool shouldDeadStop) {
 			if (!SoundtrackAPI.SoundtrackEnabled)
 				return true;
-			int level = GM.TNH_Manager.m_level;
 			//In the code, musicIndex 0 is the take theme and 1 is the hold theme.
 			if (musicIndex == 1) {
-				var holdMusic = SoundtrackAPI.GetAudioclipsForHold(level);
-				if(holdMusic.Intro.Length > 0)
-					TnHSoundtrack.Queue(holdMusic.Intro[Random.Range(0, holdMusic.Intro.Length)]); // Intro
-				if (holdMusic.Phase.Count != 0) { // Using phases.
-					for (int phase = 0; phase < holdMusic.Phase.Count; phase++) {
-						TnHSoundtrack.Queue(holdMusic.Phase[phase][Random.Range(0, holdMusic.Phase.Count)]); // Phase
-						TnHSoundtrack.Queue(holdMusic.PhaseTransition[phase][Random.Range(0, holdMusic.PhaseTransition.Count)]); // Phase Transition
+				//HoldMusic should be initialized by now in Patch_Start_AddOrbTouchSong
+				if(HoldMusic == null)
+					PluginMain.DebugLog.LogError("Failed to initialize soundtrack hold data! What did you do!?");
+				if(HoldMusic.Intro.Length > 0)
+					TnHSoundtrack.Queue(HoldMusic.Intro[Random.Range(0, HoldMusic.Intro.Length)]); // Intro
+				if (HoldMusic.Phase.Count != 0) { // Using phases.
+					for (int phase = 0; phase < HoldMusic.Phase.Count; phase++) {
+						TnHSoundtrack.Queue(HoldMusic.Phase[phase][Random.Range(0, HoldMusic.Phase.Count)]); // Phase
+						TnHSoundtrack.Queue(HoldMusic.PhaseTransition[phase][Random.Range(0, HoldMusic.PhaseTransition.Count)]); // Phase Transition
 					}
 				}
 				else { // Not using Phases. Continue as normal.
-					TnHSoundtrack.Queue(holdMusic.Lo[Random.Range(0, holdMusic.Lo.Length)]); // Lo
-					if(holdMusic.Transition.Length > 0)
-						TnHSoundtrack.Queue(holdMusic.Transition[Random.Range(0, holdMusic.Transition.Length)]); // Transition
-					if(holdMusic.MedHi.Length > 0)
-						TnHSoundtrack.Queue(holdMusic.MedHi[Random.Range(0, holdMusic.MedHi.Length)]); // MedHi
+					TnHSoundtrack.Queue(HoldMusic.Lo[Random.Range(0, HoldMusic.Lo.Length)]); // Lo
+					if(HoldMusic.Transition.Length > 0)
+						TnHSoundtrack.Queue(HoldMusic.Transition[Random.Range(0, HoldMusic.Transition.Length)]); // Transition
+					if(HoldMusic.MedHi.Length > 0)
+						TnHSoundtrack.Queue(HoldMusic.MedHi[Random.Range(0, HoldMusic.MedHi.Length)]); // MedHi
 				}
-				if(holdMusic.End.Length > 0)
-					TnHSoundtrack.Queue(holdMusic.End[Random.Range(0, holdMusic.End.Length)]); // End
-				var take = SoundtrackAPI.GetAudioclipsForTake(level + 1);
+				if(HoldMusic.End.Length > 0)
+					TnHSoundtrack.Queue(HoldMusic.End[Random.Range(0, HoldMusic.End.Length)]); // End
+				var take = SoundtrackAPI.GetAudioclipsForTake(GM.TNH_Manager.m_level + 1);
 				TnHSoundtrack.Queue(take.Track, "loop", take.Name, "take"); // Take song after the hold
 				
 				TnHSoundtrack.PlayNextSongInQueue(); // Plays next song, finishing Take and playing Intro (if exists, if not, Lo or Phases.)
@@ -105,6 +109,29 @@ namespace TNHBGLoader.Soundtrack {
 		public static bool Patch_Start_AddTnHSoundtrack(ref TNH_Manager __instance) {
 			if(SoundtrackAPI.SoundtrackEnabled)
 				__instance.gameObject.AddComponent<TnHSoundtrack>();
+			return true;
+		}
+		
+		//Implement OrbTouch funzies
+		[HarmonyPatch(typeof(TNH_HoldPointSystemNode), "Start")]
+		[HarmonyPrefix]
+		public static bool Patch_Start_AddOrbTouchSong(ref TNH_HoldPointSystemNode __instance) {
+			if (!SoundtrackAPI.SoundtrackEnabled)
+				return true;
+			//Initialize holdmusic. This SHOULD be performed before Patch_SwitchTo_PlaySoundtrackSongs
+			HoldMusic = SoundtrackAPI.GetAudioclipsForHold(GM.TNH_Manager.m_level);
+
+			//If there's no OrbTouch, skip this
+			if (HoldMusic.OrbTouch.Length == 0)
+				return true;
+			
+			//Convert tracks to a list of audioclips
+			var clips = new List<AudioClip>();
+			foreach (var track in HoldMusic.OrbTouch)
+				clips.Add(track.clip);
+			//Rewrite the AudioEvent's sound for OrbTouch.
+			__instance.AUDEvent_HoldActivate.Clips = clips;
+			//it's uncanny how easy it is
 			return true;
 		}
 	}
