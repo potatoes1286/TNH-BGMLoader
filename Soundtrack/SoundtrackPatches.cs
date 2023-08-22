70a03ae87aee159cbd7f29695c3cbd56a63e3e21
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2023-08-22 16:59:13 -0400

    See previous commit

--- Soundtrack/SoundtrackPatches.cs
+++ Soundtrack/SoundtrackPatches.cs
@@ -11,15 +11,24 @@ namespace TNHBGLoader.Soundtrack {
 			if (!SoundtrackAPI.SoundtrackEnabled)
 				return true;
 			int level = GM.TNH_Manager.m_level;
+			//In the code, musicIndex 0 is the take theme and 1 is the hold theme.
 			if (musicIndex == 1) {
 				var holdMusic = SoundtrackAPI.GetAudioclipsForHold(level);
 				if(holdMusic.Intro.Length > 0)
-					TnHSoundtrack.Queue(holdMusic.Intro[Random.Range(0, holdMusic.Intro.Length)]); // Forcefully set song to intro, which will end and go to
-				TnHSoundtrack.Queue(holdMusic.Lo[Random.Range(0, holdMusic.Intro.Length)]); // The Lo song, which will need to be manually skipped to
-				if(holdMusic.Transition.Length > 0)
-					TnHSoundtrack.Queue(holdMusic.Transition[Random.Range(0, holdMusic.Intro.Length)]); // The transition song which ends and starts
-				if(holdMusic.MedHi.Length > 0)
-					TnHSoundtrack.Queue(holdMusic.MedHi[Random.Range(0, holdMusic.Intro.Length)]); // The MedHi song, see Lo and then
+					TnHSoundtrack.Queue(holdMusic.Intro[Random.Range(0, holdMusic.Intro.Length)]); // Intro
+				if (holdMusic.Phase.Count != 0) { // Using phases.
+					for (int phase = 0; phase < holdMusic.Phase.Count; phase++) {
+						TnHSoundtrack.Queue(holdMusic.Phase[phase][Random.Range(0, holdMusic.Phase.Count)]); // Phase
+						TnHSoundtrack.Queue(holdMusic.PhaseTransition[phase][Random.Range(0, holdMusic.PhaseTransition.Count)]); // Phase Transition
+					}
+				}
+				else { // Not using Phases. Continue as normal.
+					TnHSoundtrack.Queue(holdMusic.Lo[Random.Range(0, holdMusic.Lo.Length)]); // Lo
+					if(holdMusic.Transition.Length > 0)
+						TnHSoundtrack.Queue(holdMusic.Transition[Random.Range(0, holdMusic.Transition.Length)]); // Transition
+					if(holdMusic.MedHi.Length > 0)
+						TnHSoundtrack.Queue(holdMusic.MedHi[Random.Range(0, holdMusic.MedHi.Length)]); // MedHi
+				}
 				if(holdMusic.End.Length > 0)
 					TnHSoundtrack.Queue(holdMusic.End[Random.Range(0, holdMusic.End.Length)]); // End
 				var take = SoundtrackAPI.GetAudioclipsForTake(level + 1);
@@ -28,6 +37,20 @@ namespace TNHBGLoader.Soundtrack {
 				TnHSoundtrack.PlayNextSongInQueue(); // Plays next song, finishing Take and playing Intro (if exists, if not, Lo or Phases.)
 			}
 			return false;
+		}
+		
+		[HarmonyPatch(typeof(TNH_HoldPoint), "BeginPhase")]
+		[HarmonyPrefix]
+		public static bool Patch_BeginPhase_HandlePhases() {
+			if (!SoundtrackAPI.SoundtrackEnabled)
+				return true;
+			
+			//If the next song is NOT a phase, just skip this whole bit.
+			//This also handles phase overflow! if it overlfows, the next song would be End and itll just keep playing the highest phase until the actual end
+			if (!TnHSoundtrack.SongQueue[0].type.Contains("phasetr"))
+				return true;
+			TnHSoundtrack.PlayNextSongInQueue();
+			return true;
 		}
 		
 		[HarmonyPatch(typeof(TNH_Manager), "SetHoldWaveIntensity")]
