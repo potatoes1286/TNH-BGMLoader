de8695e1cfdecb9ceb95bb6f190f8f6441e1ef53
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2023-10-08 14:46:58 -0400

    Fix announcer padding (#11)

--- Announcer/AnnouncerYamlfest.cs
+++ Announcer/AnnouncerYamlfest.cs
@@ -1,19 +1,21 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
 using FistVR;
 using JetBrains.Annotations;
-using UnityEngine.Internal;
 using YamlDotNet.Serialization;
 
-namespace TNHBGLoader
-{
-	public class AnnouncerYamlfest
-	{
-		public             string Name         { get; set; }
-		[YamlMember(Alias = "Guid")]
-		public string GUID { get;                     set; }
-		public             string VoiceLines   { get; set; }
-		[CanBeNull] public string Location     { get; set; }
-		public             float  FrontPadding { get; set; } = 0.2f;
-		public             float  BackPadding  { get; set; } = 1.2f;
+namespace TNHBGLoader {
+	public class AnnouncerYamlfest {
+		public string Name { get; set; }
+
+		[YamlMember(Alias = "Guid")] public string GUID { get; set; }
+
+		public string VoiceLines { get; set; }
+
+		[CanBeNull] public string Location { get; set; }
+
+		[DefaultValue(0.2f)] public float FrontPadding { get; set; }
+
+		[DefaultValue(1.2f)] public float BackPadding { get; set; }
 	}
 }
